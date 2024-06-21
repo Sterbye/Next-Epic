8e1b20dd72786f01c012a518c5d29ab8d27ca50c
Damian Simonak <simonak.damian123@gmail.com>
2024-06-21 15:00:50 +0200

    Layout building

--- src/components/custom/HeroSection.tsx
+++ src/components/custom/HeroSection.tsx
@@ -33,8 +33,7 @@ export function HeroSection({ data }: Readonly<HeroSectionProps>) {
         alt="Background"
         className="absolute inset-0 object-cover w-full h-full"
         height={1080}
-        src="https://images.pexels.com/photos/4050314/pexels-photo-4050314.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        // style={image.url}
+        src={image.url}
         width={1920}
       />
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white bg-black bg-opacity-20">
