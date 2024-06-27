61039c4369d85fbfe963b7e1a091e2372902f519
Damian Simonak <simonak.damian123@gmail.com>
2024-06-27 13:57:19 +0200

    Add midleware, custom buttons, auth, error handling

--- src/components/forms/SignUpForm.tsx
+++ src/components/forms/SignUpForm.tsx
@@ -16,6 +16,8 @@ import {
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { ZodErrors } from "@/components/custom/ZodErrors";
+import { StrapiErrors } from "@/components/custom/StrapiErrors";
+import { SubmitButton } from "@/components/custom/SubmitButton";
 
 const INITIAL_STATE = {
   data: null,
@@ -71,7 +73,12 @@ export function SignupForm() {
             </div>
           </CardContent>
           <CardFooter className="flex flex-col">
-            <button className="w-full">Sign Up</button>
+            <SubmitButton
+              className="w-full"
+              text="Sign Up"
+              loadingText="Loading"
+            />
+            <StrapiErrors error={formState?.strapiErrors} />
           </CardFooter>
         </Card>
         <div className="mt-4 text-center text-sm">
